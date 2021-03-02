884c2d90d979bf6ddb1482e3031cdad9f05b2214
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-02 13:21:26 -0800

    still need to figure out how to run conditional who wins from total points' git status clear

---
+++ js/main.js
@@ -0,0 +1,50 @@
+document.querySelector('button').addEventListener('click', getCards)
+
+
+let level1;
+let level2;
+let name1;
+let name2;
+
+function getCards(){
+let random = Math.floor(Math.random() * 880);
+let url = `https://pokeapi.co/api/v2/pokemon/${random}`
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    
+    name1 = data.forms[0].name
+    let pic1 = data.sprites.other['official-artwork'].front_default;
+    level1 = Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat + data.stats[4].base_stat + data.stats[5].base_stat)
+    document.querySelector('#pokeImg1').src = pic1
+    document.querySelector('h2').innerHTML = name.toUpperCase()
+    document.querySelector('#firstScore').innerHTML = `Your total score is ${level1}`
+
+  })
+  .catch(err => {
+    console.log(`err ${err}`)
+  })
+
+let random2 = Math.floor(Math.random() * 880)
+let url2 = `https://pokeapi.co/api/v2/pokemon/${random2}`
+
+fetch(url2)
+  .then(res => res.json())
+  .then(data => {
+    console.log(Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat ))
+    name2 = data.forms[0].name;
+    let pic2 = data.sprites.other['official-artwork'].front_default
+    level2 = Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat + data.stats[4].base_stat + data.stats[5].base_stat)
+    document.querySelector('h3').innerHTML = name2.toUpperCase()
+    document.querySelector('#pokeImg2').src = pic2
+    document.querySelector('#secondScore').innerText = `Your total score is ${level2}`
+  })
+  .catch(err => {
+    console.log(`err ${err}`)
+  });
+
+}
+
+document.querySelector('button').addEventListener('click', getWinner)
+
