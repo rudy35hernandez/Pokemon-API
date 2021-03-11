e8f678d44a7467e96a39fb783b3b4c8d5187d6a5
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-10 18:14:03 -0800

    added some styling' clear

--- js/main.js
+++ js/main.js
@@ -64,6 +64,7 @@ fetch(url1)
     let pic1 = data.sprites.other['official-artwork'].front_default;
     level1 = Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat + data.stats[4].base_stat + data.stats[5].base_stat)
     document.querySelector('#pokeImg1').src = pic1
+    document.querySelector('#pokeImg1').style.visibility = "visible";
     document.querySelector('h2').innerHTML = name1.toUpperCase()
     document.querySelector('#firstScore').innerHTML = `Your total score is ${level1}`
     console.log(level1)
@@ -87,11 +88,14 @@ fetch(url2)
     level2 = Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat + data.stats[4].base_stat + data.stats[5].base_stat)
     document.querySelector('h3').innerHTML = name2.toUpperCase()
     document.querySelector('#pokeImg2').src = pic2
+    document.querySelector('#pokeImg2').style.visibility = "visible";
     document.querySelector('#secondScore').innerText = `Your total score is ${level2}`
     if(level1 > level2){
-      document.querySelector('h5').innerHTML = `${name1} wins the battle`
+      document.querySelector('h5').innerText = `${name1} wins the battle!`
     } else if(level2 > level1){
-      document.querySelector('h5').innerHTML = `${name2} wins the battle`
+      document.querySelector('h5').innerText = `${name2} wins the battle!`
+    } else {
+      document.querySelector('h5').innerText = 'This battle is a draw!'
     }
   
 })
@@ -105,5 +109,4 @@ fetch(url2)
 
 
   
-console.log('This should log first')
 
