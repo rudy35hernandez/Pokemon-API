7d081da400fe5629162d1817957048eeb4289e73
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2021-03-17 18:14:39 -0700

    simplified fetch, added await code

--- js/main.js
+++ js/main.js
@@ -1,90 +1,38 @@
-/*document.querySelector('button').addEventListener('click', getCards)
-
-let level1;
-let level2;
-let name1;
-let name2;
-
-function getCards(){
-let random = Math.floor(Math.random() * 880);
-let url = `https://pokeapi.co/api/v2/pokemon/${random}`
-
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    
-    name1 = data.forms[0].name
-    let pic1 = data.sprites.other['official-artwork'].front_default;
-    level1 = Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat + data.stats[4].base_stat + data.stats[5].base_stat)
-    document.querySelector('#pokeImg1').src = pic1
-    document.querySelector('h2').innerHTML = name1.toUpperCase()
-    document.querySelector('#firstScore').innerHTML = `Your total score is ${level1}`
-
-  })
-  .catch(err => {
-    console.log(`err ${err}`)
-  })
-
-let random2 = Math.floor(Math.random() * 880)
-let url2 = `https://pokeapi.co/api/v2/pokemon/${random2}`
-
-fetch(url2)
-  .then(res => res.json())
-  .then(data => {
-    console.log(Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat ))
-    name2 = data.forms[0].name;
-    let pic2 = data.sprites.other['official-artwork'].front_default
-    level2 = Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat + data.stats[4].base_stat + data.stats[5].base_stat)
-    document.querySelector('h3').innerHTML = name2.toUpperCase()
-    document.querySelector('#pokeImg2').src = pic2
-    document.querySelector('#secondScore').innerText = `Your total score is ${level2}`
-    
-  })
-  .catch(err => {
-    console.log(`err ${err}`)
-  });
-
-  }*/
+/// Added the simplified version of fetch!!!!
 
   document.querySelector('button').addEventListener('click', getCards)
 
+
 let level1;
 let level2;
 let name1;
 let name2;
 
-function getCards(){
+async function getCards(){
+  /// Around 880 pokemon, creating randomized picker
 let random = Math.floor(Math.random() * 880);
 let url1 = `https://pokeapi.co/api/v2/pokemon/${random}`
 
-fetch(url1)
-  .then((res) => {
-  res.json().then((data) =>{
-    name1 = data.forms[0].name
-    let pic1 = data.sprites.other['official-artwork'].front_default;
-    level1 = Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat + data.stats[4].base_stat + data.stats[5].base_stat)
-    document.querySelector('#pokeImg1').src = pic1
-    document.querySelector('#pokeImg1').style.visibility = "visible";
-    document.querySelector('h2').innerHTML = name1.toUpperCase()
-    document.querySelector('#firstScore').innerHTML = `Your total score is ${level1}`
-    console.log(level1)
+const res = await fetch(url1)
+const data = await res.json()
+name1 = data.forms[0].name
+let pic1 = data.sprites.other['official-artwork'].front_default;
+level1 = Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat + data.stats[4].base_stat + data.stats[5].base_stat)
+document.querySelector('#pokeImg1').src = pic1
+document.querySelector('#pokeImg1').style.visibility = "visible";
+document.querySelector('h2').innerHTML = name1.toUpperCase()
+document.querySelector('#firstScore').innerHTML = `Your total score is ${level1}`
+console.log(level1)
 
-})
-}) //prints 35
-  .catch(err => {
-    console.log(`err ${err}`)
-  })
 
 let random2 = Math.floor(Math.random() * 880)
 let url2 = `https://pokeapi.co/api/v2/pokemon/${random2}`
 
-fetch(url2)
-  .then((res) => {
-  res.json()
-  .then((data) =>{
-    name2 = data.forms[0].name;
-    let pic2 = data.sprites.other['official-artwork'].front_default
-    level2 = Number(data.stats[0].base_stat + data.stats[1].base_stat + data.stats[2].base_stat + data.stats[3].base_stat + data.stats[4].base_stat + data.stats[5].base_stat)
+const res2 = await fetch(url2)
+const data2 = await res2.json()
+name2 = data2.forms[0].name;
+    let pic2 = data2.sprites.other['official-artwork'].front_default
+    level2 = Number(data2.stats[0].base_stat + data2.stats[1].base_stat + data2.stats[2].base_stat + data2.stats[3].base_stat + data2.stats[4].base_stat + data2.stats[5].base_stat)
     document.querySelector('h3').innerHTML = name2.toUpperCase()
     document.querySelector('#pokeImg2').src = pic2
     document.querySelector('#pokeImg2').style.visibility = "visible";
@@ -97,16 +45,6 @@ fetch(url2)
     } else if(level1 == level2){
       document.querySelector('h5').innerText = 'This battle is a draw!'
     }
-  
-})
-}) 
-  .catch(err => {
-    console.log(`err ${err}`)
-  });
-
-  
-}
-
-
+  }
   
 
